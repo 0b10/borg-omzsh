637ba6be26fdc029127787b6a8122541a09bedc2
0b10 <0b10@pm.me>
2019-11-16 22:49:51 +0000

    Move the config to .config/bb-omzsh
       ...so that changes can be pulled to the repo, without erasing the
          config. it makes sense that the config is in an actual
          configuration directory

--- borgbackup.plugin.zsh
+++ borgbackup.plugin.zsh
@@ -1,16 +1,25 @@
 #!/usr/bin/zsh
 
 __BB_LOCAL_DIR="$(dirname $0)/"
-__BB_CONFIG_FILE="${__BB_LOCAL_DIR}/borgbackup.config.zsh"
+__BB_CONFIG_TEMPLATE="${__BB_LOCAL_DIR}/borgbackup.config.template"
+__BB_CONFIG_FILE="${HOME}/.config/bb-omzsh/borgbackup.config.zsh"
 
-source "$__BB_CONFIG_FILE"
+# if not e, if naugthy..
+if [[ ! -e "$__BB_CONFIG_FILE" ]]; then
+    # make a config under .config/bb-omzsh
+    mkdir -p `dirname ${__BB_CONFIG_FILE}`
+    cp ${__BB_CONFIG_TEMPLATE} ${__BB_CONFIG_FILE};
+fi
+
+source "$__BB_CONFIG_FILE";
+
 
 function __bb_help() {
-    echo "help menu"
+    echo "help menu";
 }
 
 function __bb_backup() {
-    borg "${__BB_OPTIONS[@]}" create "${BORG_REPO}"::"$__BB_ARCHIVE_NAME" "${__BB_TARGETS[@]}"
+    borg "${__BB_OPTIONS[@]}" create "${BORG_REPO}"::"$__BB_ARCHIVE_NAME" "${__BB_TARGETS[@]}";
 }
 
 function __bb_edit() {
@@ -21,7 +30,7 @@ function __bb_edit() {
     fi
 }
 
-function bb () {
+function bb() {
     [[ -z `command -v borg` ]] && echo "you must install borgbackup first" && return 1
     [[ -z $BORG_REPO ]] && echo "you must export BORG_REPO from your zshrc first" && return 1
     
