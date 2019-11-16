d4d60c2bf3b30e9ea968f8944680d6e1dd32b9d2
0b10 <0b10@pm.me>
2019-11-16 15:16:55 +0000

    Make config file constant

--- bb.plugin.zsh
+++ bb.plugin.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/zsh
 
+BB_CONFIG_FILE="./bb-config.zsh"
+
+source "$BB_CONFIG_FILE"
+
 function __help() {
     echo "help menu"
 }
@@ -8,14 +12,23 @@ function __backup() {
     echo "backup"
 }
 
+function __set_repo() {
+    echo "setting repo"
+}
+
 case "$1" in
     "backup"|"-b"|"--backup")
-        __backup
+        __backup;
+    ;;
+    "setrepo"|"-r"|"--setrepo"|"--set-repo")
+        __set_repo;
     ;;
     "help"|"-h"|"--help")
-        __help
+        __help;
     ;;
     *)
         echo "invalid command '$1'";
     ;;
 esac
+
+unset BB_CONFIG_FILE;
