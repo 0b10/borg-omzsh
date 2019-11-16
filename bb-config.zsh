e3fe0e89fa56cae3597fa492dbe72e1cc8a5b1a4
0b10 <0b10@pm.me>
2019-11-16 16:11:18 +0000

    WIP: Create add option

--- bb-config.zsh
+++ bb-config.zsh
@@ -1,3 +1,13 @@
 #!/usr/bin/zsh
 
 export BORG_REPO="~/.backup/borg"
+
+__BB_TARGETS=(
+    "/foo/bar"
+    "/foo/bar2"
+)
+
+# __B_TARGETS=(
+#     "/foo/bar"
+#     "/foo/bar2"
+# )
--- bb.plugin.zsh
+++ bb.plugin.zsh
@@ -1,48 +1,53 @@
 #!/usr/bin/zsh
 
-__LOCAL_DIR="$(dirname $0)/"
-__BB_CONFIG_FILE="${__LOCAL_DIR}/bb-config.zsh"
+__BB_LOCAL_DIR="$(dirname $0)/"
+__BB_CONFIG_FILE="${__BB_LOCAL_DIR}/bb-config.zsh"
 
 source "$__BB_CONFIG_FILE"
 
 function __exit() {
     unset __BB_CONFIG_FILE;
-    unset __LOCAL_DIR;
+    unset __BB_LOCAL_DIR;
+    unset __BB_TARGET;
     exit $1
 }
 
-function __help() {
+function __bb_help() {
     echo "help menu"
 }
 
-function __backup() {
+function __bb_backup() {
     echo "backup"
 }
 
-function __set_repo() {
+function __bb_set_repo() {
     # $1 is the repo path
     [[ -z $1 ]] && echo "you must provide a repo path" && __exit 1
     sed -i "/^export BORG_REPO=/c\export BORG_REPO=\"${1}\"" "$__BB_CONFIG_FILE"
 }
 
-function __add_target() {
+function __bb_add_target() {
+    # $1 is the target path to include in backups
     [[ -z $1 ]] && echo "you must provide a target path" && __exit 2
-    echo "add target"
+    __BB_TARGETS+=($1)
+    for item in "${__BB_TARGETS[@]}"; do
+        echo $item
+    done;
 }
 
 
 case "$1" in
     "backup"|"-b"|"--backup")
-        __backup;
+        __bb_backup;
     ;;
     "add"|"-a"|"--add")
-        __add_target;
+        __bb_add_target $2;
     ;;
     "setrepo"|"-r"|"--setrepo"|"--set-repo")
-        __set_repo $2; # pass in repo path
+        __bb_set_repo $2; # pass in repo path
     ;;
     "help"|"-h"|"--help")
-        __help;
+        __bb_help;
     ;;
     *)
         echo "invalid command '$1'";
