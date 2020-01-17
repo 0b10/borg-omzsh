69a835845cd61bdc0893201b73b3f91ac241c5ab
0b10 <0b10@pm.me>
2020-01-17 01:32:46 +0000

    feat: implement store archiving

--- borgbackup.plugin.zsh
+++ borgbackup.plugin.zsh
@@ -13,6 +13,11 @@ fi
 
 source "$__BB_CONFIG_FILE";
 
+# set and make BORG_REPO (but doesn't init it)
+__BB_STORE="${__BB_BACKUP_DIR}/${HOSTNAME}"
+export BORG_REPO="${__BB_STORE}/borg"
+[[ -e $BORG_REPO ]]  || mkdir -p $BORG_REPO || return 1;
+
 function __bb_help() {
     echo -e "Usage: bb [OPTION]";
     
@@ -31,9 +36,26 @@ function __bb_help() {
     echo -e ""
 }
 
+__bb_archive() {
+    local archive_name="${HOSTNAME}.tar.gz"
+    local store_name=${HOSTNAME};
+    (
+        cd ${__BB_BACKUP_DIR};
+        
+        # shred old archive
+        [[ -e ${archive_name} ]] && echo "shredding old archive: ${archive_name}.." && shred -un 1 ${archive_name};
+        
+        # create new archive
+        echo "creating new archive: ${archive_name}.." && tar --gzip --create --file=${archive_name} ${store_name} && \
+        # print file size
+        echo archive created @ $((`stat -c %s ${archive_name}`/(1024*1024)))MB
+    )
+}
+
 function __bb_backup() {
     source $__BB_CONFIG_FILE; # because it's a script, and stale state between shells can occur
-    borg "${__BB_OPTIONS[@]}" create "${BORG_REPO}"::"$__BB_ARCHIVE_NAME" "${__BB_TARGETS[@]}";
+    borg "${__BB_OPTIONS[@]}" create "${BORG_REPO}"::"$__BB_ARCHIVE_NAME" "${__BB_TARGETS[@]}" && \
+    ([[ $__SHOULD_ARCHIVE == true ]] && __bb_archive)
 }
 
 function __bb_edit() {
@@ -46,7 +68,8 @@ function __bb_edit() {
 
 function bb() {
     [[ -z `command -v borg` ]] && echo "you must install borgbackup first" && return 1;
-    [[ -z $BORG_REPO ]] && echo "you must export BORG_REPO from your zshrc first" && return 1;
+    [[ -z $__BB_BACKUP_DIR ]] && echo "you must set __BB_BACKUP_DIR in the config file - use 'bb edit'" && return 1;
+    [[ -z $__SHOULD_ARCHIVE ]] && echo "you must set __SHOULD_ARCHIVE in the config file - use 'bb edit'" && return 1;
     
     case "$1" in
         ""|"backup"|"-b"|"--backup")
